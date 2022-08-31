e250a434ab939b15a1d7e9e17ecef5900c4f90c2
msaivikas <msaivikas7@gmail.com>
2022-08-31 16:49:36 +0530

    created models. ready to create views and urls

--- base/models.py
+++ base/models.py
@@ -1,2 +1,34 @@
 from django.db import models
 
+
+class Complaint(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.RESTRICT, null=True, blank=True) # one complaint<->one student but one student<->many complaints; On deleting a complaint, student shouldn't be deleted.
+    GROUP =(
+        ('wf', 'Wifi'),
+        ('et', 'Electrical'),
+        ('ts', 'Toilets'),
+        ('cp', 'Carpentry'),
+        ('cl', 'Cleaning'),
+        ('os', 'Others'),
+    )
+    group = models.CharField(max_length=2, choices=GROUP,)
+    quick_text = models.CharField(max_length=100,)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    reported_date = models.DateField(auto_now_add=True)
+    resolved_date = models.DateField(null=True, blank=True)
+    resolved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.student
+
+    class Meta:
+        ordering = ['-reported_date']
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=100,)
+    hostel = models.CharField(max_length=100)
+    room = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.student_name
