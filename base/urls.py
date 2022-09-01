d2b91f296e1018c3ed6cedca0da83f3d5f68fed7
msaivikas <msaivikas7@gmail.com>
2022-09-01 22:50:48 +0530

    num_complaints working ; class based views are being used

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import ComplaintList
+from .views import ComplaintList, ComplaintAdd
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('', ComplaintList.as_view(), name = 'complaint_list'),
+    path('complaint-add', ComplaintAdd.as_view(), name = 'complaint_add'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
+import imp
 from django.shortcuts import render
 
 from .models import Complaint, Student
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 class ComplaintList(ListView):
     model = Complaint
-    num_complaints = model.objects.all().count()
+    # num_complaints = model.objects.all().count()
     context_object_name = 'complaints'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_complaints'] = context['complaints'].all().count()
+        return context
+
+class ComplaintAdd(CreateView):
+    model = Complaint
+    fields = ['student.student_name', 'group', 'quick_text', 'description',]
+    success_url = reverse_lazy('complaint_list')
+
