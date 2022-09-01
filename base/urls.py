fc6b645337e739655b4216fc695cd7c7fc0a9577
msaivikas <msaivikas7@gmail.com>
2022-09-01 20:41:58 +0530

    urls and views modified a little. only the homepage is created at this point. *num_complaints isn't visible, gotta correct this.

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
+from .views import ComplaintList
 
 urlpatterns = [
-    
+    # path('', views.index, name='index'),
+    path('', ComplaintList.as_view(), name = 'complaint_list'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Complaint, Student
+from django.views.generic.list import ListView
+
+class ComplaintList(ListView):
+    model = Complaint
+    num_complaints = model.objects.all().count()
+    context_object_name = 'complaints'
